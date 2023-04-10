67836a17668ffedf418835b24704d847d2694000
Hugo Delautre <97909616+Gautatyr@users.noreply.github.com>
2023-04-10 07:52:18 +0200

    Add project files.

---
+++ ShiftsLoggerAPI/Models/Shift.cs
@@ -0,0 +1,13 @@
+﻿namespace ShiftsLoggerAPI.Models;
+
+public class Shift
+{
+    public int Id { get; set; }
+    DateTime start { get; set; }
+    DateTime end { get; set; }
+
+    public TimeSpan duration()
+    {
+        return end - start;
+    }
+}
